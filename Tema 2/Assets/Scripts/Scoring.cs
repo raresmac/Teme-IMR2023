977c4fab7c7fa4fe791ac181f7d92af98e5c114b
Rares Macovei <rares_mac1@yahoo.com>
2023-10-19 10:06:25 +0300

    Preparing Tema2

---
+++ Tema 2/Assets/Scripts/Scoring.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    public AudioSource sound;
+    void OnTriggerEnter(Collider collider)
+    {
+        sound.Play(0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

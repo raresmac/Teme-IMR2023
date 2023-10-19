96271fa36cbe7de545d4826315c2da4f674f5bd4
Rares Macovei <rares_mac1@yahoo.com>
2023-10-19 11:33:31 +0300

    Added Score

--- Tema 2/Assets/Scripts/Scoring.cs
+++ Tema 2/Assets/Scripts/Scoring.cs
@@ -1,21 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Scoring : MonoBehaviour
 {
-    public AudioSource sound;
-    void OnTriggerEnter(Collider collider)
+    // to be put in Basketball -> XR Grab Interactable -> OnSelectExited
+    // and removed from Basketball
+    public TextMeshPro score;
+    public TextMeshPro lastShot;
+    public TextMeshPro bestShot;
+    private float maxDistance = 0;
+    private int scoreNumber = 0;
+    private AudioSource sound;
+    private Transform target;
+    private Transform positionAtThrow;
+    new public Collider collider;
+    private float distance;
+
+
+    void OnTriggerEnter()
     {
-        sound.Play(0);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        if(collider.isTrigger){
+            sound.Play(0);  
+            scoreNumber++;
+            score.text = "SCORE: " + scoreNumber;
+            lastShot.text = "Last shot: " + distance;
+            if(distance > maxDistance){
+                maxDistance = distance;
+                bestShot.text = "Best shot: " + distance;
+            }
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Object was thrown");
+        sound = GameObject.FindWithTag("MonkeySound").GetComponent<AudioSource>();
+        target = GameObject.FindWithTag("Hoops").transform;
+        positionAtThrow = GameObject.FindWithTag("Basketball").transform;
+
+        distance = Vector3.Distance(target.position, positionAtThrow.position);
+    }
+
     void Update()
     {
         

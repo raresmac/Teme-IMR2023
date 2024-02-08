4569fe954fe382677bbc00c16949f961639aa7d1
Rares Macovei <rares_mac1@yahoo.com>
2024-02-09 01:32:20 +0200

    Working colliders on glass

--- Proiect_Final/Assets/Puzzles/Puzzles/Heart-Shaped Box/ActivateGravity.cs
+++ Proiect_Final/Assets/Puzzles/Puzzles/Heart-Shaped Box/ActivateGravity.cs
@@ -1,6 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ActivateGravity : MonoBehaviour
 {
@@ -15,14 +16,21 @@ public class ActivateGravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetMouseButtonDown(1)) {
-                
-            }
-            // anim.SetBool("button", false);
+
     }
 
     private void OnCollisionEnter(Collision collision){
-        collision.gameObject.transform.parent.GetComponentInChildren<Rigidbody>().useGravity = true;
-        // GetComponent<Rigidbody>().useGravity = true;
+        if(this.transform.parent.tag != "Finish" && collision.gameObject.tag == "GameController"){
+            this.GetComponent<Rigidbody>().useGravity = true;
+            // foreach(Transform child in transform.parent.transform){
+                // if(child.name == "Heart") continue;
+                // child.GetComponent<Rigidbody>().useGravity = true;
+            // }
+            this.transform.parent.tag = "Finish";
+            Debug.Log("Activated gravity!");
+        }
+        else if(collision.gameObject.tag == "GameController" && Time.timeScale > 0.3f){
+            SceneManager.LoadScene(sceneName:"Heart-Shaped Box");
+        }
     }
 }
--- Proiect_Final/Assets/Puzzles/Puzzles/Heart-Shaped Box/SlowTime.cs
+++ Proiect_Final/Assets/Puzzles/Puzzles/Heart-Shaped Box/SlowTime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class SlowTime : MonoBehaviour
 {
     public GameObject RightController;
     private Vector3 lastPos;
+    private float elapsed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,16 @@ public class SlowTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = RightController.transform.position;
-        float distance = Vector3.Distance(lastPos, newPos);
-        Time.timeScale = 0.2f + distance / 10;
-        lastPos = RightController.transform.position;
+        elapsed += Time.deltaTime;
+        if (elapsed > 1f / 15)
+        {
+            elapsed = 0f;
+            Vector3 newPos = RightController.transform.position;
+            float distance = Vector3.Distance(lastPos, newPos);
+            Time.timeScale = Math.Min(1, 0.2f + distance / 2);
+            // Debug.Log("Time is now " + Time.timeScale);
+            lastPos = RightController.transform.position;
+            // Debug.Log("LastPos is now " + lastPos);
+        }
     }
 }

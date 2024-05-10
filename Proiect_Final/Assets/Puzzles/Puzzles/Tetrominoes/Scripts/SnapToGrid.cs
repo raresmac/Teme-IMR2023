7e706d06786857dc86424170e1e798ee63026544
Rares Macovei <rares_mac1@yahoo.com>
2024-05-10 17:05:09 +0300

    maybe final version tetrocube

--- Proiect_Final/Assets/Puzzles/Puzzles/Tetrominoes/Scripts/SnapToGrid.cs
+++ Proiect_Final/Assets/Puzzles/Puzzles/Tetrominoes/Scripts/SnapToGrid.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -5,83 +6,104 @@ public class SnapToGrid : MonoBehaviour
 {
     // Reference to the Grid component
     public TetroGrid tetroGrid;
+    private Tetromino tetromino;
 
     // Maximum distance to snap to the grid
     public float maxSnapDistance = 0.5f;
-    public Vector3 snapOffset = Vector3.zero;
 
     // Reference to the XR Grab Interactable component
     private XRGrabInteractable grabInteractable;
 
-    void Start()
-    {
+    void Start(){
         // Get the XR Grab Interactable component attached to the object
         grabInteractable = GetComponent<XRGrabInteractable>();
 
+        // Get the Tetromino component attached to the object
+        tetromino = GetComponent<Tetromino>();
+
         // Subscribe to the XR Interaction Manager's events
-        if (grabInteractable != null)
-        {
+        if (grabInteractable != null){
+            grabInteractable.selectEntered.AddListener(OnSelectEntered);
             grabInteractable.selectExited.AddListener(OnSelectExited);
         }
 
         // If the Grid component is not assigned, try to find it in the scene
-        if (tetroGrid == null)
-        {
+        if (tetroGrid == null){
             tetroGrid = FindObjectOfType<TetroGrid>();
 
             // If Grid component is not found, log a warning
-            if (tetroGrid == null)
-            {
+            if (tetroGrid == null){
                 Debug.LogError("Grid component not found. Make sure to assign it in the inspector or add it to the scene.");
             }
         }
     }
+    
+    void Update(){
+        // Check if the right mouse button is pressed
+        // if (Input.GetMouseButtonDown(1)){
+        //     Snap();
+        // }
+    }
 
-    void OnSelectExited(SelectExitEventArgs args)
-    {
+    void OnSelectEntered(SelectEnterEventArgs args){
+        // Free the grid cells occupied by the object before moving it if it was placed
+        if (tetromino.isPlaced){
+            tetromino.FreeCells(tetroGrid);
+        }
+    }
+
+    void OnSelectExited(SelectExitEventArgs args){
         // Snap the object to the grid after releasing it
         Snap();
     }
 
-    void Snap()
-    {
-        // Calculate the nearest grid cell position based on the cells from the TetroGrid component
-        float minDistance = Mathf.Infinity;
-        Vector3 minPosition = Vector3.zero;
-        for(int i = 0; i < tetroGrid.gridSizeX; i++){
-            for(int j = 0; j < tetroGrid.gridSizeY; j++){
-                for(int k = 0; k < tetroGrid.gridSizeZ; k++){
-                    if (testAllChildren()){
-                        Vector3 cellPosition = tetroGrid.getGridCube(i, j, k).cubeCell.transform.position;
-                        if(Vector3.Distance(transform.position + snapOffset, cellPosition) < minDistance){
-                            minDistance = Vector3.Distance(transform.position + snapOffset, cellPosition);
-                            minPosition = cellPosition;
-                        }
-                    }
-                }
-            }
+    void Snap(){
+        CubeCell foundCubeCell = tetroGrid.getCellCoords(transform.position);
+        if (foundCubeCell == null || foundCubeCell.isOccupied){
+            return;
         }
 
-        if (minDistance <= maxSnapDistance){
-            changePosRos(minPosition);
+        Vector3 minPosition = foundCubeCell.cubeCell.transform.position;
+        float minDistance = Vector3.Distance(transform.position, minPosition);
+
+        if (minDistance <= maxSnapDistance && TestAllChildren()){
+            Debug.Log("Snap to grid");
+            ChangePosRos(minPosition);
         }
     }
 
-    void changePosRos(Vector3 minPosition){
-        {
-            transform.position = minPosition;
-            float newRotationX = Mathf.Round(transform.rotation.eulerAngles.x / 90) * 90;
-            float newRotationY = Mathf.Round(transform.rotation.eulerAngles.y / 90) * 90;
-            float newRotationZ = Mathf.Round(transform.rotation.eulerAngles.z / 90) * 90;
-            transform.rotation = Quaternion.Euler(newRotationX, newRotationY, newRotationZ);
+    void ChangePosRos(Vector3 minPosition){
+        transform.position = minPosition;
+        float newRotationX = Mathf.Round(transform.rotation.eulerAngles.x / 90) * 90;
+        float newRotationY = Mathf.Round(transform.rotation.eulerAngles.y / 90) * 90;
+        float newRotationZ = Mathf.Round(transform.rotation.eulerAngles.z / 90) * 90;
+        transform.rotation = Quaternion.Euler(newRotationX, newRotationY, newRotationZ);
+
+        // If all the children are inside the grid and the cells are free, occupy them
+        TetrominoCube[] blocks =  tetromino.tetrominoCubes;
+        foreach(TetrominoCube block in blocks){
+            CubeCell foundCubeCell = tetroGrid.getCellCoords(block.transform.position);
+            Vector3 coords = foundCubeCell.cubeCell.transform.position;
+            block.setPlacedTrue(foundCubeCell);
         }
+
+        tetromino.isPlaced = true;
     }
 
-    bool testAllChildren(){
-        Transform[] blocks = GetComponentsInChildren<Transform>();
-        foreach(Transform block in blocks){
+    bool TestAllChildren(){
+        TetrominoCube[] blocks =  tetromino.tetrominoCubes;
+        
+        // Check if all the children are inside the grid and the cells are free
+        foreach(TetrominoCube block in blocks){
             // Debug.Log(block.position);
-            if (!tetroGrid.isInsideGrid(block.position)){
+            CubeCell foundCubeCell = tetroGrid.getCellCoords(block.transform.position);
+            if (foundCubeCell == null){
+                block.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+                return false;
+            }
+            Vector3 coords = foundCubeCell.cubeCell.transform.position;
+            if (coords == new Vector3(-1, -1, -1) || foundCubeCell.isOccupied){
+                block.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
                 return false;
             }
         }
--- Proiect_Final/Assets/Puzzles/Puzzles/Tetrominoes/Scripts/TetroGrid.cs
+++ Proiect_Final/Assets/Puzzles/Puzzles/Tetrominoes/Scripts/TetroGrid.cs
@@ -7,6 +7,9 @@ public class TetroGrid : MonoBehaviour
     public int gridSizeY = 5;
     public int gridSizeZ = 3;
     public GameObject instantiated;
+    public Material occupiedMaterial;
+    public Material freeMaterial;
+
 
     // Size of each grid cell
     public float cellSize = 0.5f;
@@ -33,8 +36,24 @@ public class TetroGrid : MonoBehaviour
         CreateGrid();
     }
 
-    void CreateGrid()
+    void Update()
     {
+        // Check if the puzzle is solved
+        if(CheckSolution()){
+            Debug.Log("Puzzle solved!");
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                for (int y = 0; y < gridSizeY; y++)
+                {
+                    for (int z = 0; z < gridSizeZ; z++){
+                        gridCubes[x][y][z].occupier.GetComponent<MeshRenderer>().material.color = Color.green;
+                    }
+                }
+            }
+        }
+    }
+
+    void CreateGrid(){
         // Loop through each cell in the grid
         for (int x = 0; x < gridSizeX; x++)
         {
@@ -42,7 +61,7 @@ public class TetroGrid : MonoBehaviour
             {
                 for (int z = 0; z < gridSizeZ; z++){
                     // Create a new cube in the cell
-                    createCube(x, y, z);
+                    CreateCube(x, y, z);
 
                     // Debug.Log("Drawn cube at: " + gridCubes[x][y][z]);
                 }
@@ -59,53 +78,105 @@ public class TetroGrid : MonoBehaviour
         // Debug.Log("Boundaries y: " + gridCubes[0][0][0].boundaries[1, 0] + " " + gridCubes[gridSizeX - 1][gridSizeY - 1][gridSizeZ - 1].boundaries[1, 1]);
 
         boundaries[2, 0] = gridCubes[0][0][0].boundaries[2, 0];
-        boundaries[2, 1] = gridCubes[gridSizeX - 1][gridSizeY - 1][gridSizeZ - 1].boundaries[0, 1];
+        boundaries[2, 1] = gridCubes[gridSizeX - 1][gridSizeY - 1][gridSizeZ - 1].boundaries[2, 1];
         // Debug.Log("Boundaries z: " + gridCubes[0][0][0].boundaries[2, 0] + " " + gridCubes[gridSizeX - 1][gridSizeY - 1][gridSizeZ - 1].boundaries[2, 1]);
     }
 
-    void createCube(int x, int y, int z){
+    void CreateCube(int x, int y, int z){
         // Instantiate the cube
-        gridCubes[x][y][z].cubeCell = Instantiate(instantiated, this.transform.localPosition, Quaternion.Euler(0, 0, 0), this.transform);
+        gridCubes[x][y][z].cubeCell = Instantiate(instantiated, transform.localPosition, Quaternion.Euler(0, 0, 0), transform);
+        gridCubes[x][y][z].tetroGrid = this;
+        gridCubes[x][y][z].x = x;
+        gridCubes[x][y][z].y = y;
+        gridCubes[x][y][z].z = z;
 
         // Set the position and scale of the cube
         gridCubes[x][y][z].cubeCell.transform.localPosition = new Vector3(x * cellSize, y * cellSize, z * cellSize);
         gridCubes[x][y][z].cubeCell.transform.localScale = new Vector3(cellSize, cellSize, cellSize);
 
         // Set the boundaries of the cube
-        gridCubes[x][y][z].boundaries[0, 0] = gridCubes[x][y][z].cubeCell.transform.position.x - cellSize / 10;
-        gridCubes[x][y][z].boundaries[0, 1] = gridCubes[x][y][z].cubeCell.transform.position.x + cellSize / 10;
-        gridCubes[x][y][z].boundaries[1, 0] = gridCubes[x][y][z].cubeCell.transform.position.y - cellSize / 10;
-        gridCubes[x][y][z].boundaries[1, 1] = gridCubes[x][y][z].cubeCell.transform.position.y + cellSize / 10;
-        gridCubes[x][y][z].boundaries[2, 0] = gridCubes[x][y][z].cubeCell.transform.position.z - cellSize / 10;
-        gridCubes[x][y][z].boundaries[2, 1] = gridCubes[x][y][z].cubeCell.transform.position.z + cellSize / 10;
+        gridCubes[x][y][z].boundaries[0, 0] = gridCubes[x][y][z].cubeCell.transform.localPosition.x - cellSize / 2;
+        gridCubes[x][y][z].boundaries[0, 1] = gridCubes[x][y][z].cubeCell.transform.localPosition.x + cellSize / 2;
+        gridCubes[x][y][z].boundaries[1, 0] = gridCubes[x][y][z].cubeCell.transform.localPosition.y - cellSize / 2;
+        gridCubes[x][y][z].boundaries[1, 1] = gridCubes[x][y][z].cubeCell.transform.localPosition.y + cellSize / 2;
+        gridCubes[x][y][z].boundaries[2, 0] = gridCubes[x][y][z].cubeCell.transform.localPosition.z - cellSize / 2;
+        gridCubes[x][y][z].boundaries[2, 1] = gridCubes[x][y][z].cubeCell.transform.localPosition.z + cellSize / 2;
+        // Debug.Log("Boundaries for cube "+ x + y + z + ": " + gridCubes[x][y][z].boundaries[0, 0] + " " + gridCubes[x][y][z].boundaries[0, 1] + " " + gridCubes[x][y][z].boundaries[1, 0] + " " + gridCubes[x][y][z].boundaries[1, 1] + " " + gridCubes[x][y][z].boundaries[2, 0] + " " + gridCubes[x][y][z].boundaries[2, 1]);
     }
 
-    public CubeCell getGridCube(int x, int y, int z){
+    public CubeCell GetGridCube(int x, int y, int z){
         if (x < 0 || x >= gridSizeX || y < 0 || y >= gridSizeY || z < 0 || z >= gridSizeZ){
             return null;
         }
         return gridCubes[x][y][z];
     }
 
-    public bool isInsideGrid(Vector3 position){
+    public bool IsInsideGrid(Vector3 position){
         return position.x >= boundaries[0, 0] && position.x <= boundaries[0, 1] &&
                position.y >= boundaries[1, 0] && position.y <= boundaries[1, 1] &&
                position.z >= boundaries[2, 0] && position.z <= boundaries[2, 1];
     }
 
-    public bool isCellFree(int x, int y, int z){
+    public bool IsCellOccupied(int x, int y, int z){
+        if(GetGridCube(x, y, z) == null){
+            return true;
+        }
         return gridCubes[x][y][z].isOccupied;
+    }
+
+    public CubeCell getCellCoords(Vector3 position){
+        position -= transform.position;
+        if (!IsInsideGrid(position)){
+            // Debug.Log("Position is not inside the grid");
+            return null;
+        }
+        int cubeCellX = (int)Mathf.Round(position.x / cellSize);
+        int cubeCellY = (int)Mathf.Round(position.y / cellSize);
+        int cubeCellZ = (int)Mathf.Round(position.z / cellSize);
+        return gridCubes[cubeCellX][cubeCellY][cubeCellZ];
+    }
+
+    bool CheckSolution(){
+        // Check if the grid is full
+        for (int x = 0; x < gridSizeX; x++){
+            for (int y = 0; y < gridSizeY; y++){
+                for (int z = 0; z < gridSizeZ; z++){
+                    if (!gridCubes[x][y][z].isOccupied){
+                        gridCubes[x][y][z].cubeCell.GetComponent<MeshRenderer>().material = freeMaterial;
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // If the grid is full, the puzzle is solved
+        for (int x = 0; x < gridSizeX; x++){
+            for (int y = 0; y < gridSizeY; y++){
+                for (int z = 0; z < gridSizeZ; z++){
+                    gridCubes[x][y][z].occupier.gameObject.GetComponent<MeshRenderer>().material = occupiedMaterial;
+                }
+            }
+        }
+        return true;
     }
 }
 
 public class CubeCell
 {
     public GameObject cubeCell;
+    public TetroGrid tetroGrid;
     public bool isOccupied = false;
+    public TetrominoCube occupier = null;
     public float[,] boundaries = new float[3,2];
+    public int x, y, z;
 
-    void Start()
-    {
-        // Initialize the boundaries matrix
+    public void setOcuppiedTrue(TetrominoCube occupier){
+        isOccupied = true;
+        this.occupier = occupier;
+    }
+
+    public void setOcuppiedFalse(){
+        isOccupied = false;
+        this.occupier = null;
     }
 }
